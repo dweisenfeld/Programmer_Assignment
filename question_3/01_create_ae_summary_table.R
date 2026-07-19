98aadc048e2242591dda5c815cc23cde97195194
dweisenfeld <dlweisenfeld@gmail.com>
2026-07-19 18:47:36 +0000

    question 3 part 1 - create treatment-emergent AE table

---
+++ question_3/01_create_ae_summary_table.R
@@ -0,0 +1,55 @@
+##==============================================================================
+# Purpose: Create and save summary table of treatment-emergent AEs (TEAEs)
+#
+# Auth: DLW
+# Date: 7/19/2026
+#
+# Input datasets
+#   - pharmaverseadam::adae 
+#.  - pharmaverseadam::adsl
+# 
+# Deliverables
+# - Script to create summary table: question_3_tlg/01_create_ae_summary_table.R
+# - Text files/log files as evidence for code running error-free
+# - ae_summary_table.html (or .docx/.pdf)
+#
+# Notes:
+# Summary Table using {gtsummary} - HINT - FDA Table 10
+# Create a summary table of treatment-emergent adverse events (TEAEs).
+# - Treatment-emergent AE records will have TRTEMFL == "Y" in pharmaverseadam::adae
+# - Rows: AETERM or AESOC
+# - Columns: Treatment groups (ACTARM)
+# - Cell values: Count (n) and percentage (%)
+# - Include total column with all subjects
+# - Sort by descending frequency
+##==============================================================================
+
+library(gtsummary)
+library(gt)
+library(dplyr)
+
+## Load data
+adae <- pharmaverseadam::adae 
+adsl <- pharmaverseadam::adsl
+
+## Pre-process data
+teae <- adae |>
+  filter(TRTEMFL == "Y") |> # treatment emergent AE records 
+  select(USUBJID, AETERM, AESOC, ACTARM)
+
+## Create table  
+teae_tbl <- teae |>
+  tbl_hierarchical(
+    variables = c(AESOC, AETERM),
+    by = ACTARM,
+    id = USUBJID,
+    denominator = adsl,
+    overall_row = TRUE,
+    label = "..ard_hierarchical_overall.." ~ "Any SAE"
+  ) |>
+  sort_hierarchical(sort = "descending")
+
+## Save table
+teae_tbl |> 
+  as_gt() |> 
+  gtsave(filename = "question_3/output/ae_summary_table.html")
